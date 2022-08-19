023f9fff2918cafb11146b4e91177a204d617909
Shwetha <104769790+Shwetha2008@users.noreply.github.com>
2022-08-19 20:03:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var canvas = new fabric.Canvas("myCanvas");
+var canvas = new fabric.Canvas('myCanvas');
 
  block_y=1;
  block_x=1;
@@ -6,7 +6,7 @@ var canvas = new fabric.Canvas("myCanvas");
 block_image_width = 350;
 block_image_height = 430;
 
-var block_image_object= "";
+var block_image_object= " ";
 
 function new_image(get_image)
 {
@@ -24,7 +24,7 @@ function new_image(get_image)
 
         canvas.add(block_image_object);
 	});
-    }
+}
 
 
 window.addEventListener("keydown", my_keydown);
@@ -36,32 +36,32 @@ console.log(keyPressed);
 
 	if(keyPressed == '82') 
 	{
-		new_image = ("rr1.png");
+		new_image("rr.jpg");
 		console.log("r");
 	}
 	if(keyPressed == '71')
 	{
 		block_x = 200;
-		new_image = ("gr.png");
+		new_image("gr.png");
 		console.log("g");	
 	}
 	
 	if(keyPressed == '89')
 	{
 		block_x =350;
-		new_image = ("yr.png");
+		new_image("yr.png");
 		console.log("y");
 	}
 	if(keyPressed == '80')
 	{
 		block_x = 600;
-		new_image = ("pr.png");
+		new_image("pr.png");
 		console.log("p");
 	}
 	if(keyPressed == '66')
 	{
 		block_x = 700;
-		new_image = ("br.png");
+		new_image("br.png");
 		console.log("b");
 		
 	}
